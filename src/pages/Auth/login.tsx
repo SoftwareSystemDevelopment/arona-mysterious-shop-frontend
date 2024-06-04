8c4699719b0ceb978fe60165bf5ad0211b1dc214
yurzhang <yurzhang.oi@gmail.com>
2024-06-04 14:48:10 +0800

    refa: login page

--- src/pages/Auth/login.tsx
+++ src/pages/Auth/login.tsx
@@ -1,69 +1,80 @@
-/// TODO(yur): review this file
-
-import { Card } from "~/components";
+import { createSignal } from "solid-js";
+import { A } from "@solidjs/router";
+import { Button, Card } from "~/components";
 
 export default () => {
+  const [username, setUsername] = createSignal("");
+  const [password, setPassword] = createSignal("");
+  const [remember, setRemember] = createSignal(false);
+
+  // TODO: login
+  const onLogin = () => {
+    console.log(`Username: ${username()}`);
+    console.log(`Password: ${password()}`);
+    console.log(`Remember: ${remember()}`);
+  };
+
   return (
-    <Card class="py-20">
-      <h1 class="py-5 text-center text-3xl">登录</h1>
-      <form class="mx-auto max-w-sm">
-        <div class="mb-5">
-          <label
-            for="email"
-            class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            用户名
-          </label>
-          <input
-            id="email"
-            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
-        <div class="mb-5">
-          <label
-            for="password"
-            class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-          >
-            密码
-          </label>
-          <input
-            type="password"
-            id="password"
-            class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-            required
-          />
-        </div>
-        <div class="mb-5 flex items-start justify-between">
-          <div class="flex h-5 items-center">
-            <input
-              id="remember"
-              type="checkbox"
-              value=""
-              class="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-            />
-            <label
-              for="remember"
-              class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              记住我
-            </label>
-          </div>
-          <button
-            // onClick={() => setType("register")}
-            class="ms-2 text-sm font-medium text-blue-500 underline hover:text-blue-900 dark:text-gray-300"
-          >
-            没有账号？前往注册
-          </button>
-        </div>
-        <button
-          type="submit"
-          class="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    <div class="flex justify-center">
+      <Card class="flex w-4/5 max-w-[1080px] flex-col items-center space-y-4 py-8">
+        <h1 class="text-3xl">用户登录</h1>
+        <form
+          class="flex w-3/5 flex-col space-y-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onLogin();
+          }}
         >
-          登录
-        </button>
-      </form>
-    </Card>
+          <div class="flex items-center space-x-4">
+            <label for="username" class="font-medium text-gray-900">
+              用户名
+            </label>
+            <input
+              id="username"
+              class="flex-1 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+              placeholder="Username"
+              onInput={(e) => setUsername(e.currentTarget.value)}
+              value={username()}
+              required
+            />
+          </div>
+          <div class="flex items-center space-x-4">
+            <label for="password" class="font-medium text-gray-900">
+              密码
+            </label>
+            <input
+              type="password"
+              id="password"
+              class="flex-1 rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+              placeholder="Password"
+              onInput={(e) => setPassword(e.currentTarget.value)}
+              value={password()}
+              required
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="flex items-center space-x-1">
+              <input
+                id="remember"
+                type="checkbox"
+                class="border border-gray-300 bg-gray-50 focus:ring-blue-300"
+                onInput={(e) => setRemember(e.currentTarget.checked)}
+                checked={remember()}
+              />
+              <label for="remember" class="text-sm font-medium text-gray-900">
+                记住我
+              </label>
+            </div>
+            <span class="text-sm font-medium">
+              还没有账号？前往
+              <A class="text-blue-500 hover:text-blue-900" href="/register">
+                注册
+              </A>
+            </span>
+          </div>
+          <Button type="submit">登录</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
