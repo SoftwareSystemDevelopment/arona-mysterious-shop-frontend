2aa60eedeb6a6bc4da388d12e1349de2a15865b0
AerVento <wz407021500@163.com>
2024-06-06 14:37:52 +0800

    Revert "fix: 将页面拆分到多个路由上"
    This reverts commit c01f8b679a868884943e8670b002d27672f369ef.

--- src/pages/UserCenter/Order.tsx
+++ src/pages/UserCenter/Order.tsx
@@ -1,6 +1,5 @@
 import { A } from "@solidjs/router";
 import { Card } from "~/components";
-import { UserCenter } from "./center";
 
 const OrderItem = (props: {
   orderId: string;
@@ -50,53 +49,51 @@ const OrderItem = (props: {
   );
 };
 
-export default () => {
+export const OrderTag = () => {
   return (
-    <UserCenter type="Order">
-      <div>
-        <div class="w-full">
-          <h1 class="py-5 pl-10 text-2xl font-semibold">订单信息</h1>
-        </div>
-        <hr />
-        <div class="my-8">
-          <ul>
-            <li class="flex rounded-t-xl bg-blue-600 p-3 text-lg font-semibold text-white">
-              <div class="flex w-[15%] px-2 text-center">
-                <h1 class="my-auto w-full">订单ID</h1>
-              </div>
-              <div class="flex w-[15%] px-2 text-center">
-                <h1 class="my-auto w-full">订单状态</h1>
-              </div>
-              <div class="flex w-[15%] px-2 text-center">
-                <h1 class="my-auto w-full">店铺名称</h1>
-              </div>
-              <div class="flex w-[55%] px-2 text-center">
-                <h1 class="my-auto w-full">商品信息</h1>
-              </div>
-            </li>
-            <Card>
-              <OrderItem
-                orderId="123"
-                orderStatus="待付款"
-                shopId="123"
-                shopName="店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称"
-                goodId="123"
-                title="商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题"
-                cover="/arona-mysterious-shop-frontend/img3.webp"
-              />
-              <OrderItem
-                orderId="123"
-                orderStatus="待付款"
-                shopId="123"
-                shopName="店铺名称"
-                goodId="123"
-                title="商品标题"
-                cover="/arona-mysterious-shop-frontend/img2.webp"
-              />
-            </Card>
-          </ul>
-        </div>
+    <div>
+      <div class="w-full">
+        <h1 class="py-5 pl-10 text-2xl font-semibold">订单信息</h1>
       </div>
-    </UserCenter>
+      <hr />
+      <div class="my-8">
+        <ul>
+          <li class="flex rounded-t-xl bg-blue-600 p-3 text-lg font-semibold text-white">
+            <div class="flex w-[15%] px-2 text-center">
+              <h1 class="my-auto w-full">订单ID</h1>
+            </div>
+            <div class="flex w-[15%] px-2 text-center">
+              <h1 class="my-auto w-full">订单状态</h1>
+            </div>
+            <div class="flex w-[15%] px-2 text-center">
+              <h1 class="my-auto w-full">店铺名称</h1>
+            </div>
+            <div class="flex w-[55%] px-2 text-center">
+              <h1 class="my-auto w-full">商品信息</h1>
+            </div>
+          </li>
+          <Card>
+            <OrderItem
+              orderId="123"
+              orderStatus="待付款"
+              shopId="123"
+              shopName="店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称店铺名称"
+              goodId="123"
+              title="商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题"
+              cover="/arona-mysterious-shop-frontend/img3.webp"
+            />
+            <OrderItem
+              orderId="123"
+              orderStatus="待付款"
+              shopId="123"
+              shopName="店铺名称"
+              goodId="123"
+              title="商品标题"
+              cover="/arona-mysterious-shop-frontend/img2.webp"
+            />
+          </Card>
+        </ul>
+      </div>
+    </div>
   );
 };
