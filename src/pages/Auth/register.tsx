68c14a06d4e78fb93a26a2bdc84037b31ec0182f
AerVento <wz407021500@163.com>
2024-06-11 09:46:16 +0800

    update: 添加了注册的toggle

--- src/pages/Auth/register.tsx
+++ src/pages/Auth/register.tsx
@@ -73,7 +73,14 @@ export default () => {
             value={passwd2()}
             required
           />
-          <div class="flex flex-row-reverse">
+          <div class="flex justify-between">
+            <label class="inline-flex cursor-pointer items-center">
+              <input type="checkbox" value="" class="peer sr-only" />
+              <div class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
+              <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+                我是商家
+              </span>
+            </label>
             <span class="text-sm font-medium">
               已经注册？前往
               <A class="text-blue-500 hover:text-blue-900" href="/login">
