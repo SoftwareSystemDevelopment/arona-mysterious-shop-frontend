d33a1dd2d612d6e0427764c1d40b5c6f4979aea9
yurzhang <yurzhang.oi@gmail.com>
2024-06-13 15:27:34 +0800

    feat: good image

--- src/components/GoodBrief/index.tsx
+++ src/components/GoodBrief/index.tsx
@@ -1,17 +1,46 @@
-import { createMemo } from "solid-js";
+import { Match, Switch, createMemo } from "solid-js";
 import { A } from "@solidjs/router";
-import { GoodInfo } from "~/data/interface";
+import { QueryFunction, createQuery } from "@tanstack/solid-query";
+import { GoodInfo, Response } from "~/data/interface";
+
+const queryImageFn: QueryFunction<string> = async (props) => {
+  const resp = await fetch(
+    `/api/image/get/product?productId=${props.queryKey[2]}`,
+  );
+
+  const res: Response<string> = await resp.json();
+
+  if (res.data === null) {
+    throw new Error(res.message);
+  }
+
+  return `/api/${res.data}`;
+};
 
 interface GoodBriefProps extends GoodInfo {}
 
 export default (props: GoodBriefProps) => {
   const price = createMemo(() => Math.round(props.productPrice * 100) / 100);
 
+  const query = createQuery(() => ({
+    queryKey: ["image", "good", props.productId],
+    queryFn: queryImageFn,
+  }));
+
   return (
     <div class="m-2 flex w-[240px] flex-col items-center space-y-2 rounded-md bg-white/20 p-4 hover:bg-white/40">
       <A href={`/goods/${props.productId}`}>
-        {/* TODO: good image */}
-        <img src="/arona-mysterious-shop-frontend/img1.webp" />
+        <Switch>
+          <Match when={query.isPending}>
+            <img src="/arona-mysterious-shop-frontend/img1.webp" />
+          </Match>
+          <Match when={query.isError}>
+            <img src="/arona-mysterious-shop-frontend/img1.webp" />
+          </Match>
+          <Match when={query.isSuccess}>
+            <img src={query.data} />
+          </Match>
+        </Switch>
       </A>
       <A class="line-clamp-3 text-xl" href={`/goods/${props.productId}`}>
         {props.productName}
--- src/pages/GoodDetails/Details.tsx
+++ src/pages/GoodDetails/Details.tsx
@@ -1,23 +1,55 @@
-import { createMemo, createSignal } from "solid-js";
-import { GoodInfo } from "~/data/interface";
+import { Match, Switch, createMemo, createSignal } from "solid-js";
+import { QueryFunction, createQuery } from "@tanstack/solid-query";
+import { GoodInfo, Response } from "~/data/interface";
 import { Button, Card } from "~/components";
+
+const queryImageFn: QueryFunction<string> = async (props) => {
+  const resp = await fetch(
+    `/api/image/get/product?productId=${props.queryKey[2]}`,
+  );
+
+  const res: Response<string> = await resp.json();
+
+  if (res.data === null) {
+    throw new Error(res.message);
+  }
+
+  return `/api/${res.data}`;
+};
 
 interface DetailsProps extends GoodInfo {}
 
 export default (props: DetailsProps) => {
   const price = createMemo(() => Math.round(props.productPrice * 100) / 100);
 
+  const query = createQuery(() => ({
+    queryKey: ["image", "good", props.productId],
+    queryFn: queryImageFn,
+  }));
+
   const [count, setCount] = createSignal(0);
 
   return (
-    <div class="flex justify-center space-x-4">
-      <Card class="flex w-3/5 max-w-[810px] py-8">
-        <div class="flex-[3]">
-          {/* TODO: good image */}
-          <img
-            class="rounded-md"
-            src="/arona-mysterious-shop-frontend/img1.webp"
-          />
+    <div class="flex w-4/5 max-w-[1080px] space-x-4">
+      <Card class="flex flex-[3] py-8">
+        <div class="flex flex-[3] justify-center">
+          <Switch>
+            <Match when={query.isPending}>
+              <img
+                class="rounded-md"
+                src="/arona-mysterious-shop-frontend/img1.webp"
+              />
+            </Match>
+            <Match when={query.isError}>
+              <img
+                class="rounded-md"
+                src="/arona-mysterious-shop-frontend/img1.webp"
+              />
+            </Match>
+            <Match when={query.isSuccess}>
+              <img class="rounded-md" src={query.data} />
+            </Match>
+          </Switch>
         </div>
         <div class="flex flex-[2] flex-col justify-between px-4 py-2">
           <div class="space-y-2">
@@ -29,7 +61,7 @@ export default (props: DetailsProps) => {
           </div>
         </div>
       </Card>
-      <Card class="flex w-1/5 max-w-[270px] flex-col justify-around py-8">
+      <Card class="flex flex-1 flex-col justify-around py-8">
         <div class="flex space-x-2">
           <button
             onClick={() => setCount((c) => c - 1)}
