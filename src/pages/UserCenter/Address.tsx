6d6771dfc49ee1f04c78344001e4629f8cf4e677
yurzhang <yurzhang.oi@gmail.com>
2024-06-13 22:58:15 +0800

    feat: add address

--- src/pages/UserCenter/Address.tsx
+++ src/pages/UserCenter/Address.tsx
@@ -1,11 +1,12 @@
-import { For, Match, Show, Switch } from "solid-js";
+import { For, Match, Show, Switch, createSignal } from "solid-js";
 import {
   createMutation,
   createQuery,
   useQueryClient,
 } from "@tanstack/solid-query";
 import { AddressInfo, Response } from "~/data/interface";
-import { Button } from "~/components";
+import { Button, Input } from "~/components";
+import { createStore } from "solid-js/store";
 
 interface AddressItemProps extends AddressInfo {}
 
@@ -63,6 +64,35 @@ export default () => {
     queryFn,
   }));
 
+  const [forCreate, setForCreate] = createSignal(false);
+
+  const [form, setForm] = createStore({
+    addressName: "",
+    receiver: "",
+    userPhone: "",
+  });
+
+  const client = useQueryClient();
+
+  const mutation = createMutation(() => ({
+    mutationFn: async () => {
+      const resp = await fetch("/api/address/add", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(form),
+      });
+      const res = await resp.json();
+      if (res.data === null) {
+        throw new Error(res.message);
+      }
+    },
+    onSuccess: () => {
+      setForCreate(false);
+      setForm({ addressName: "", receiver: "", userPhone: "" });
+      client.invalidateQueries({ queryKey: ["address"] });
+    },
+  }));
+
   return (
     <div class="space-y-4 p-2">
       <h1 class="text-2xl font-semibold">收货地址</h1>
@@ -118,12 +148,45 @@ export default () => {
         </tbody>
         <tfoot>
           <tr class="bg-white/40">
-            <td class="border p-2" colSpan={4}>
-              <div class="flex justify-end">
-                {/* TODO: add address */}
-                <Button>添加收货地址</Button>
-              </div>
-            </td>
+            <Show
+              when={forCreate()}
+              fallback={
+                <td class="border p-2" colSpan={4}>
+                  <div class="flex justify-end">
+                    <Button onClick={() => setForCreate(true)}>
+                      添加收货地址
+                    </Button>
+                  </div>
+                </td>
+              }
+            >
+              <td class="border p-2">
+                <Input
+                  onInput={(e) => setForm("receiver", e.currentTarget.value)}
+                  value={form.receiver}
+                />
+              </td>
+              <td class="border p-2">
+                <Input
+                  onInput={(e) => setForm("userPhone", e.currentTarget.value)}
+                  value={form.userPhone}
+                />
+              </td>
+              <td class="border p-2">
+                <Input
+                  onInput={(e) => setForm("addressName", e.currentTarget.value)}
+                  value={form.addressName}
+                />
+              </td>
+              <td class="border p-2 text-center">
+                <button
+                  class="h-8 w-8 rounded-full bg-green-700 text-center text-white hover:bg-green-800 focus:outline-none"
+                  onClick={() => mutation.mutate()}
+                >
+                  +
+                </button>
+              </td>
+            </Show>
           </tr>
         </tfoot>
       </table>
